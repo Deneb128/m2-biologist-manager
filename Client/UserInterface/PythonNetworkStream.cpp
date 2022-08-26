4e0425ef11a17cd8e7ea1894a48e3eaae44a019e
Roberto Inconis <robertoinconis1607@gmail.com>
2022-08-26 14:04:26 +0200

    missing instructions as comments

--- Client/UserInterface/PythonNetworkStream.cpp
+++ Client/UserInterface/PythonNetworkStream.cpp
@@ -1,6 +1,6 @@
-after:
+//after:
 			Set(HEADER_GC_AFFECT_REMOVE, CNetworkPacketHeaderMap::TPacketType(sizeof(TPacketGCAffectRemove), STATIC_SIZE_PACKET));
-add:
+//add:
 			Set(HEADER_GC_BIOLOGIST_MANAGER_OPEN,	CNetworkPacketHeaderMap::TPacketType(sizeof(TPacketGCBiologistManagerOpen), STATIC_SIZE_PACKET));
 			Set(HEADER_GC_BIOLOGIST_MANAGER_RESEARCH,	CNetworkPacketHeaderMap::TPacketType(sizeof(TPacketGCBiologistManagerSendResearch), STATIC_SIZE_PACKET));
 			
